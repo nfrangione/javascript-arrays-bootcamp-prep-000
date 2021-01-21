db94ae12ceac60ad37b3dc5c758d3ce6a1b1c798
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-21 00:44:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,13 +2,12 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var addedChocolateBar = "foo";
 
 function addElementToBeginningOfArray(chocolateBars, addedChocolateBar) {
-  chocolateBars.unshift(addedChocolateBar);
-  return chocolateBars;
+  var moreChocolateBars = [addedChocolateBar,...chocolateBars]
+  return moreChocolateBars;
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, addedChocolateBar) {
-  
-  chocolateBars = [addedChocolateBar, ...chocolateBars];
+  chocolateBars.unshift(addedChocolateBar);
   return chocolateBars;
 }
 

5678286cbfb28ff88b9afcbb162a61f1c2ecb7b9
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-21 00:10:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
+function addElementToBeginningOfArray(chocolateBars, "") {
+  chocolateBars.unshift("milkduds")
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars,) {
+  chocolateBars = ["milkyway", ...chocolateBars]
+}
